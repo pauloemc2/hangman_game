aeef43d031ebede4ee9a6c398d9f8c0be11ae681
Paulo Oliveira <paulo28.dev@gmail.com>
2024-04-29 02:38:31 -0300

    Deleted temporary file

--- usborne_series/computer_battle_games_series/1_robot_missile/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-if ord(player_try) < ord(random_char) and tries != 0:
-            print('Code >')
-            
-        elif ord(player_try) > ord(random_char) and tries != 0:
-            print('Code <')
-        
-        elif ord(player_try) == ord(random_char) and tries != 0:
-          
